e518667cfcac1de23b302da9522d5777a73fa7b8
AokiAhishatsu <xeno1701@gmail.com>
2019-02-07 20:59:00 +0100

    Implemented theme selector

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tkinter
+import tkinter as tk
+import tkinter.ttk as ttk
 import resource
 
 
@@ -6,32 +7,81 @@ def bool_to_checked_str(value: bool) -> str:
 	return "checked" if value else "unchecked"
 
 
-class MainWindow(tkinter.LabelFrame):
+class SelectWindow(ttk.Frame):
 	def __init__(self, parent, **options):
+		super(SelectWindow, self).__init__(parent, **options)
+		self.parent = parent
+
+		self.rb_var = tk.IntVar(value=0)
+		rb1 = tk.Radiobutton(self, text="tk", variable=self.rb_var, value=0, command=self.rb_select)
+		rb1.grid(column=0, row=0, sticky='nw')
+		rb2 = tk.Radiobutton(self, text="themed tk", variable=self.rb_var, value=1, command=self.rb_select)
+		rb2.grid(column=0, row=1, sticky='nw')
+
+		self.style = ttk.Style()
+		themes = self.style.theme_names()  # List of themes: https://wiki.tcl-lang.org/page/List+of+ttk+Themes
+
+		self.themes_box = tk.Listbox(self, height=len(themes))
+
+		for i, tn in enumerate(themes):
+			self.themes_box.insert(i, tn)
+		if len(themes) > 0:
+			self.themes_box.select_set(0)
+		self.themes_box.grid(column=0, row=2, sticky='nw')
+
+		btn1 = tk.Button(self, text="Button1", command=self.switch_window)
+		self.themes_box.config(state="disabled")
+		btn1.grid(column=0, row=3)
+
+		self.grid(column=0, row=0, sticky='nsew')
+
+	def rb_select(self):
+		self.themes_box.config(state="normal" if self.rb_var.get() else "disabled")
+
+	def switch_window(self):
+		self.parent.withdraw()
+		new_window = tk.Toplevel(self.parent)
+
+		if self.rb_var.get():
+			MainWindow.__bases__ = (ttk.Frame,)
+			mw = MainWindow(new_window, self.parent)
+			mw.style = self.style
+			sel = self.themes_box.curselection()[0]
+			print("Theme style {} selected - {}".format(sel, self.style.theme_names()[sel]))
+			mw.style.theme_use(mw.style.theme_names()[sel])
+		else:
+			mw = MainWindow(new_window, self.parent)
+
+
+class MainWindow(tk.Frame):
+	def __init__(self, parent, root, **options):
 		super(MainWindow, self).__init__(parent, **options)
-		meta_menu = tkinter.Menu(self)
+		meta_menu = tk.Menu(self)
+
+		parent.protocol("WM_DELETE_WINDOW", self.on_closing)
+		self.root = root
 
 		# Menu1 (Labels)
-		menu1 = tkinter.Menu(meta_menu, tearoff=False)
+		menu1 = tk.Menu(meta_menu, tearoff=False)
 		meta_menu.add_cascade(label="Menu1", menu=menu1)
 		menu1.add_command(label="Label1", command=lambda: print("Menu1: Label1 clicked"))
-		menu1.add_command(label="Label2", command=lambda: print("Menu1: Label1 clicked"))
-		menu1.add_command(label="Label3", command=lambda: print("Menu1: Label1 clicked"))
+		menu1.add_command(label="Label2", command=lambda: print("Menu1: Label2 clicked"))
+		menu1.add_command(label="Label3", command=lambda: print("Menu1: Label3 clicked"))
 
 		# Menu2 (Checkbuttons)
-		menu2 = tkinter.Menu(meta_menu)
+		menu2 = tk.Menu(meta_menu)
 		meta_menu.add_cascade(label="Menu2", menu=menu2)
-		cb1_var = tkinter.BooleanVar(value=True)
-		cb2_var = tkinter.BooleanVar()
-		cb3_var = tkinter.BooleanVar()
+		cb1_var = tk.BooleanVar(value=True)
+		cb2_var = tk.BooleanVar()
+		cb3_var = tk.BooleanVar()
 		menu2.add_checkbutton(label="Checkbutton1", variable=cb1_var, command=lambda: print("Menu2: Checkbutton1 = " + bool_to_checked_str(cb1_var.get())))
 		menu2.add_checkbutton(label="Checkbutton2", variable=cb2_var, command=lambda: print("Menu2: Checkbutton2 = " + bool_to_checked_str(cb2_var.get())))
 		menu2.add_checkbutton(label="Checkbutton3", variable=cb3_var, command=lambda: print("Menu2: Checkbutton3 = " + bool_to_checked_str(cb3_var.get())))
 
 		# Menu3 (Radiobuttons)
-		menu3 = tkinter.Menu(meta_menu)
+		menu3 = tk.Menu(meta_menu)
 		meta_menu.add_cascade(label="Menu3", menu=menu3)
-		self.rb_var = tkinter.IntVar(value=0)  # Self needed here to set a valid state.
+		self.rb_var = tk.IntVar(value=0)  # Self needed here to set a valid state.
 		menu3.add_radiobutton(label="Radiobutton1", variable=self.rb_var, value=0, command=lambda: print("Menu3: Radiobutton1 selected"))
 		menu3.add_radiobutton(label="Radiobutton2", variable=self.rb_var, value=1, command=lambda: print("Menu3: Radiobutton2 selected"))
 		menu3.add_radiobutton(label="Radiobutton3", variable=self.rb_var, value=2, command=lambda: print("Menu3: Radiobutton3 selected"))
@@ -39,39 +89,38 @@ class MainWindow(tkinter.LabelFrame):
 		self.master.config(menu=meta_menu)
 
 		# Buttons
-		btn_frame = tkinter.Frame(self)
+		btn_frame = tk.Frame(self)
 		btn_frame.grid(column=0, row=0)
-		btn1 = tkinter.Button(btn_frame, text="Button1", command=lambda: print("Button1: clicked"))
+		btn1 = tk.Button(btn_frame, text="Button1", command=lambda: print("Button1: clicked"))
 		btn1.grid(column=0, row=0)
-		btn2 = tkinter.Button(btn_frame, text="Button2", command=lambda: print("Button2: clicked"))
+		btn2 = tk.Button(btn_frame, text="Button2", command=lambda: print("Button2: clicked"))
 		btn2.grid(column=0, row=1)
-		btn3 = tkinter.Button(btn_frame, text="Button3", command=lambda: print("Button3: clicked"))
+		btn3 = tk.Button(btn_frame, text="Button3", command=lambda: print("Button3: clicked"))
 		btn3.grid(column=0, row=2)
 
 		# Checkbuttons
-		cb_frame = tkinter.Frame(self)
+		cb_frame = tk.Frame(self)
 		cb_frame.grid(column=1, row=0)
-		cb1 = tkinter.Checkbutton(cb_frame, text="Checkbutton1", variable=cb1_var, command=lambda: print("Checkbox1 = " + bool_to_checked_str(cb1_var.get())))
+		cb1 = tk.Checkbutton(cb_frame, text="Checkbutton1", variable=cb1_var, command=lambda: print("Checkbox1 = " + bool_to_checked_str(cb1_var.get())))
 		cb1.grid(column=0, row=0)
-		cb2 = tkinter.Checkbutton(cb_frame, text="Checkbutton2", variable=cb2_var, command=lambda: print("Checkbox2 = " + bool_to_checked_str(cb2_var.get())))
+		cb2 = tk.Checkbutton(cb_frame, text="Checkbutton2", variable=cb2_var, command=lambda: print("Checkbox2 = " + bool_to_checked_str(cb2_var.get())))
 		cb2.grid(column=0, row=1)
-		cb3 = tkinter.Checkbutton(cb_frame, text="Checkbutton3", variable=cb3_var, command=lambda: print("Checkbox3 = " + bool_to_checked_str(cb3_var.get())))
+		cb3 = tk.Checkbutton(cb_frame, text="Checkbutton3", variable=cb3_var, command=lambda: print("Checkbox3 = " + bool_to_checked_str(cb3_var.get())))
 		cb3.grid(column=0, row=3)
 
 		# Radiobuttons
-		rb_frame = tkinter.Frame(self)
+		rb_frame = tk.Frame(self)
 		rb_frame.grid(column=2, row=0)
-
-		rb1 = tkinter.Radiobutton(rb_frame, text="Radiobutton1", variable=self.rb_var, value=0, command=lambda: print("Radiobutton1 selected"))
+		rb1 = tk.Radiobutton(rb_frame, text="Radiobutton1", variable=self.rb_var, value=0, command=lambda: print("Radiobutton1 selected"))
 		rb1.grid(column=0, row=0)
-		rb2 = tkinter.Radiobutton(rb_frame, text="Radiobutton2", variable=self.rb_var, value=1, command=lambda: print("Radiobutton2 selected"))
+		rb2 = tk.Radiobutton(rb_frame, text="Radiobutton2", variable=self.rb_var, value=1, command=lambda: print("Radiobutton2 selected"))
 		rb2.grid(column=0, row=1)
-		rb3 = tkinter.Radiobutton(rb_frame, text="Radiobutton3", variable=self.rb_var, value=2, command=lambda: print("Radiobutton3 selected"))
+		rb3 = tk.Radiobutton(rb_frame, text="Radiobutton3", variable=self.rb_var, value=2, command=lambda: print("Radiobutton3 selected"))
 		rb3.grid(column=0, row=2)
 		rb1.invoke()
 
 		# Canvas
-		canvas1 = tkinter.Canvas(self, width=100, height=50)
+		canvas1 = tk.Canvas(self, width=100, height=50)
 		canvas1.create_line(0, 0, 100, 50, fill="blue", dash=(4, 4))
 		canvas1.create_line(0, 50, 100, 0, fill="red", dash=(4, 4))
 		canvas1.create_text(50, 25, text="Canvas")
@@ -79,44 +128,44 @@ class MainWindow(tkinter.LabelFrame):
 		canvas1.grid(column=3, row=0)
 
 		# Label
-		label1 = tkinter.Label(self, text="Label")
+		label1 = tk.Label(self, text="Label")
 		label1.grid(column=4, row=0)
 
 		# Entry
-		entry1_var = tkinter.StringVar(self, value='Entry')
-		entry1 = tkinter.Entry(self, width=6, textvariable=entry1_var)
+		entry1_var = tk.StringVar(self, value='Entry')
+		entry1 = tk.Entry(self, width=6, textvariable=entry1_var)
 		entry1.grid(column=5, row=0)
 
 		# Spinbox
-		spinbox1_text = tkinter.StringVar(value="Spinbox")
-		spinbox1 = tkinter.Spinbox(self, width=8, textvariable=spinbox1_text, command=lambda: print("Spinbox:up/down"))
+		spinbox1_text = tk.StringVar(value="Spinbox")
+		spinbox1 = tk.Spinbox(self, width=8, textvariable=spinbox1_text, command=lambda: print("Spinbox:up/down"))
 		spinbox1.grid(column=6, row=0)
 
 		# Scale
-		scale1_var = tkinter.IntVar(value=50)
-		scale1 = tkinter.Scale(self, label="Scale", variable=scale1_var, command=lambda x=str(scale1_var.get()): print("Scale:" + x))
+		scale1_var = tk.IntVar(value=50)
+		scale1 = tk.Scale(self, label="Scale", variable=scale1_var, command=lambda x=str(scale1_var.get()): print("Scale:" + x))
 		scale1.grid(column=7, row=0)
 
 		# Listbox
-		listbox1 = tkinter.Listbox(self, height=4)
+		listbox1 = tk.Listbox(self, height=4)
 		for i in range(5):
 			listbox1.insert(i, "Listbox line " + str(i + 1))
 		listbox1.grid(column=8, row=0)
 
 		# PhotoImage
-		self.photo_image = tkinter.PhotoImage(data=resource.deer_img_b64)
-		photo_label = tkinter.Label(self, image=self.photo_image)
+		self.photo_image = tk.PhotoImage(data=resource.deer_img_b64)
+		photo_label = tk.Label(self, image=self.photo_image)
 		photo_label.grid(column=0, row=1, columnspan=3)
 
 		# BitmapImage
-		self.bitmap = tkinter.BitmapImage(data=resource.x11_bmp)
-		bitmap_label = tkinter.Label(self, image=self.bitmap)
+		self.bitmap = tk.BitmapImage(data=resource.x11_bmp)
+		bitmap_label = tk.Label(self, image=self.bitmap)
 		bitmap_label.grid(column=3, row=1, columnspan=3)
 
 		# Text + Scrollbar
-		text = tkinter.Text(self, height=10, width=20)
+		text = tk.Text(self, height=10, width=20)
 		text.grid(column=6, row=1, columnspan=3, sticky='nsew')
-		scrollbar1 = tkinter.Scrollbar(self, command=text.yview)
+		scrollbar1 = tk.Scrollbar(self, command=text.yview)
 		scrollbar1.grid(row=1, column=9, sticky='nsew')
 		text['yscrollcommand'] = scrollbar1.set
 		text.insert("1.0", "Text + Scrollbar\n")
@@ -124,9 +173,9 @@ class MainWindow(tkinter.LabelFrame):
 
 		# Add weight to all cells
 		for x in range(10):
-			tkinter.Grid.columnconfigure(self, x, weight=1)
+			tk.Grid.columnconfigure(self, x, weight=1)
 		for y in range(2):
-			tkinter.Grid.rowconfigure(self, y, weight=1)
+			tk.Grid.rowconfigure(self, y, weight=1)
 
 		# Add weight to parent
 		parent.columnconfigure(0, weight=1)
@@ -134,11 +183,16 @@ class MainWindow(tkinter.LabelFrame):
 
 		self.grid(column=0, row=0, sticky='nsew')
 
+	def on_closing(self):
+		print("on_closing")
+		self.root.destroy()
+
+
 
 def main():
-	root = tkinter.Tk()
+	root = tk.Tk()
 	root.title("tk_all")
-	main_window = MainWindow(root, text="Labelframe (w/ Grid)")
+	main_window = SelectWindow(root)#, text="Labelframe (w/ Grid)")
 	root.mainloop()
 
 
